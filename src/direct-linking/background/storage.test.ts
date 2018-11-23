33775d7887b2474137256db050d965e1e1c88095
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-11-23 10:35:13 +0700

    Write some tests for various search cases
    - not fully covered yet
    - doesn't actually work because of ongoing bug with tests on the storex port branch

--- src/direct-linking/background/storage.test.ts
+++ src/direct-linking/background/storage.test.ts
@@ -71,6 +71,45 @@ const runSuite = () => {
             assertTag(tags[0], DATA.tag1)
             assertTag(tags[1], DATA.tag2)
         })
+
+        test('terms search', async () => {
+            const results = await annotationStorage.search({
+                terms: ['highlight', 'annotation', 'comment'],
+            })
+
+            expect(results).toBeDefined()
+            expect(results.length).toBe(3)
+        })
+
+        test('terms search (highlight only)', async () => {
+            const results = await annotationStorage.search({
+                terms: ['highlight', 'annotation', 'comment'],
+                highlightsOnly: true,
+            })
+
+            expect(results).toBeDefined()
+            expect(results.length).toBe(2)
+        })
+
+        test('terms search (tag filter)', async () => {
+            const results = await annotationStorage.search({
+                terms: ['highlight', 'annotation', 'comment'],
+                tagsInc: [DATA.tag1],
+            })
+
+            expect(results).toBeDefined()
+            expect(results.length).toBe(1)
+        })
+
+        test('terms search (domain filter)', async () => {
+            const results = await annotationStorage.search({
+                terms: ['highlight', 'annotation', 'comment'],
+                domainsExc: ['annotation.url'],
+            })
+
+            expect(results).toBeDefined()
+            expect(results.length).toBe(0)
+        })
     })
 
     describe('Update operations: ', () => {
